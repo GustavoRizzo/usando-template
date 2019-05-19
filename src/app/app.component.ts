7eed032c018b5dfe78933a17d5c5ad1f6322cffc
Eduardo de Albuquerque <erizzoalbuquerque@gmail.com>
2019-05-19 13:36:01 -0300

    criando diferentes ambientes: desenvolvimento, homologação e produção

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'website';
+  
+  /*Indica o ambiente que está sendo executado*/
+  public API_URL = environment.API_URL;
 }
